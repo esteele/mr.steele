6bf3c9aadc7151b6cc6e6f2c085f5081c71ecadd
Eric Steele <ems174@psu.edu>
2011-08-23 10:02:36 -0400

    Initial package structure.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '1.0a1'
+
+setup(name='mr.steele',
+      version=version,
+      description=""
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zest.releaser',
+      ],
+      entry_points={
+          # 'zest.releaser.prereleaser.before': [
+          #     'dosomething = mr.steele:some_entrypoint',
+          #     ]},
+      )
